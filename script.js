ab060fa08d74cef0baf7148bae1b86d89c8658d6
hproooo55 <roshni25dec@gmail.com>
2024-07-30 17:23:00 +0530

    fix hover effect

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ let op4 = document.querySelector(".op4");
 
 if (op1 && overlay1) {
     op1.addEventListener("mouseover", function() {
-        overlay1.style.transform = "translateY(-1px)";
+        overlay1.style.transform = "translateY(-0.1px)";
         overlay1.style.cursor = "pointer";
         overlay1.style.transition = "all 500ms ease";
     });
 }
 if (op2 && overlay2) {
     op2.addEventListener("mouseover", function() {
-        overlay2.style.transform = "translateY(-1px)";
+        overlay2.style.transform = "translateY(-0.1px)";
         overlay2.style.cursor = "pointer";
         overlay2.style.transition = "all 500ms ease";
     });
@@ -31,7 +31,7 @@ if (op2 && overlay2) {
 
 if (op3 && overlay3) {
     op3.addEventListener("mouseover", function() {
-        overlay3.style.transform = "translateY(-1px)";
+        overlay3.style.transform = "translateY(-0.1px)";
         overlay3.style.cursor = "pointer";
         overlay3.style.transition = "all 500ms ease";
     });
@@ -39,7 +39,7 @@ if (op3 && overlay3) {
 
 if (op4 && overlay4) {
     op4.addEventListener("mouseover", function() {
-        overlay4.style.transform = "translateY(-1px)";
+        overlay4.style.transform = "translateY(-0.01px)";
         overlay4.style.cursor = "pointer";
         overlay4.style.transition = "all 500ms ease";
     });
